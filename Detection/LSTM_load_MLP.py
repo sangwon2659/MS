884fc35a38863d5dd3fc33f7e0abb7044ee59f1f
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2021-06-17 11:31:51 +0900

    Update LSTM_load_MLP.py

--- Detection/LSTM_load_MLP.py
+++ Detection/LSTM_load_MLP.py
@@ -1,49 +1,70 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import keras
+from keras.layers import Dense
+from keras.layers import Dropout
 from sklearn.metrics import accuracy_score
 from tensorflow.keras.models import load_model
 from pandas import read_csv
 
-# Load data
+### Manually Preprocessing data ###
+# Dividing data into x_train, y_train, x_test and y_test
+# Data starting from 3000 to remove noise
 data = read_csv("/home/a283/DetectionAlgorithm/Data.csv", header=None)
-x_train_data = np.array(data.iloc[0:100,0:500])
+x_train_data = np.array(data.iloc[3000:12000,0:500])
 x_train_timesteps, x_train_n_features = np.shape(x_train_data)
 x_train_data = x_train_data.reshape(1, x_train_timesteps, x_train_n_features)
 
-y_train_data = np.array(data.iloc[0:100,500])
-y_train_timesteps, y_train_n_features = np.shape(y_train_data)
-y_train_data = y_train_data.reshape(1, y_train_timesteps, y_train_n_features)
+y_train_data = np.array(data.iloc[3000:12000,500])
+y_train_timesteps = len(y_train_data)
+y_train_data = y_train_data.reshape(1, 1, y_train_timesteps)
 
-x_test_data = np.array(data.iloc[100:200,0:500])
+x_test_data = np.array(data.iloc[12000:15000,0:500])
 x_test_timesteps, x_test_n_features = np.shape(x_test_data)
 x_test_data = x_test_data.reshape(1, x_test_timesteps, x_test_n_features)
 
-y_test_data = np.array(data.iloc[100:200,500])
-y_test_timesteps, y_test_n_features = np.shape(y_test_data)
-y_test_data = y_test_data.reshape(1, y_test_timesteps, y_test_n_features)
+y_test_data = np.array(data.iloc[12000:15000,500])
+y_test_timesteps = len(y_test_data)
+y_test_data = y_test_data.reshape(1, 1, y_test_timesteps)
 
-print(x_train_data)
-print(y_train_data)
-print(x_test_data)
-print(y_test_data)
-
-# Load encoder
+### Loading encoder ###
 encoder = load_model('encoder.h5')
 
-# Prediction from train and test data through the loaded encoder
-x_train_enc = encoder.predict(x_train_data)
+### Creating the MLP model ###
+# MLP with 1024 -> 512 -> 256 -> 2 layers
+model = keras.Sequential()
+model.add(keras.Input(shape=(1,64)))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(2, activation='softmax'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Extracting a summary of the model
+model.summary()
 
-x_test_enc = encoder.predict(x_test_data)
+### Fitting data into the model ###
+Training_data_row_num = 4000
+for i in range(Training_data_row_num):
+        # Reshaping data to be put into encoder
+        x_train_data_temp = x_train_data[0,i,0:500].reshape(1,1,500)
+        x_train_enc = encoder.predict(x_train_data_temp)
+        print("y value: {}".format(y_train_data[0,0,i]))
+        if y_train_data[0,0,i] == 1.0:
+                y_train_data_temp = np.array([[[1, 0]]])
+        else:
+                y_train_data_temp = np.array([[[0, 1]]])
+        model.fit(x_train_enc, y_train_data_temp, epochs = 300, verbose = 1)
 
-# Creating the MLP model
-model =
-
-# Fitting the encoded train data
-model.fit(x_train_enc, y_train)
+### Plotting history ###
+#plt.plot(history.history['loss'])
+#plt.show()
 
 # Prediction from encoded test data through the MLP model
-yhat = model.predict(x_test_enc)
+#yhat = model.predict(x_test_enc)
 
 # Printing accuracy using the y test data
-acc = accuracy_score(y_test_data, yhat)
-print(acc)
+#acc = accuracy_score(y_test_data, yhat)
+#print(acc)
