f5c8ab1f5c9c413d645e7c01a34c7dbf20562639
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2021-06-22 17:33:56 +0900

    Update LSTM_enc_train.py

--- Detection/LSTM_enc_train.py
+++ Detection/LSTM_enc_train.py
@@ -30,16 +30,27 @@ model.summary()
 model.fit(input_data, input_data, epochs=300, verbose=1)
 '''
 
+'''
+Preprocessing data
+
+input_data = input_data[0,0,0:500].reshape(1,1,500)
+print(input_data)
+
+test_data = test_data[0,0,0:500].reshape(1,1,500)
+'''
+
 ### Loading data ###
 data = read_csv("/home/a283/DetectionAlgorithm/Data.csv", header=None)
 # Data normalization
 data_norm = np.array(data)
 data_max, data_min = data_norm.max(), data_norm.min()
 max_min_diff = data_max-data_min
-data_norm = data_norm/max_min_diff
+data_norm = (data_norm/max_min_diff)*100
+# Shuffle data
+np.random.shuffle(data_norm)
 
 # Input data from 3000 to remove noise
-input_data = data_norm[3000:12000,0:500]
+input_data = data_norm[4000:4600,0:500]
 timesteps, n_features = np.shape(input_data)
 # Reshaping data fit for learning
 input_data = input_data.reshape(1, timesteps, n_features)
@@ -78,17 +89,17 @@ history = {"loss": [], "accuracy": []};
 
 ### Fitting model ###
 # Fitting each row of input data
-Training_data_row_num = 10
-epochs_ = 300
+Training_data_row_num = 600
+epochs_ = 50
 for i in range(Training_data_row_num):
-        print("---------------------------------------{}th Row".format(i))
-        # Slicing input_data row by row
-        input_data_temp = input_data[0,i,0:500].reshape(1,1,500)
-        history_temp = model.fit(input_data_temp, input_data_temp, epochs=epochs_, verbose=1)
-        # Adding history_temp data to history
-        # Extend function adds the history_temp list as single elements
-        history["loss"].extend(history_temp.history['loss'])
-        history["accuracy"].extend(history_temp.history['accuracy'])
+	print("---------------------------------------{}th Row".format(i))
+	# Slicing input_data row by row
+	input_data_temp = input_data[0,i,0:500].reshape(1,1,500)
+	history_temp = model.fit(input_data_temp, input_data_temp, epochs=epochs_, verbose=1)
+	# Adding history_temp data to history
+	# Extend function adds the history_temp list as single elements
+	history["loss"].extend(history_temp.history['loss'])
+	history["accuracy"].extend(history_temp.history['accuracy'])
 
 ### Plotting history ### 
 # Plotting loss
@@ -98,16 +109,9 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.show()
 
-# Plotting accuracy
-plt.plot(history['accuracy'])
-plt.title('Model Accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.show()
-
 ### Prediction ###
 # Slicing test_data into one row
-test_data_ = data_norm[3000,0:500].reshape(1,1,500)
+test_data_ = data_norm[4000,0:500].reshape(1,1,500)
 # Predicting using the whole model
 yhat = model.predict(test_data_, verbose=1)
 # Printing the test_data put in and the prediction from the whole model
@@ -117,5 +121,30 @@ print(yhat)
 ### Saving h5 file ###
 # Defining enc_save as a model from the input upto the encoder
 enc_save = keras.Model(inputs=visible, outputs=encoder)
-enc_save.save('encoder_.h5')
+enc_save.save('encoder.h5')
+
+for i in range(50):
+	input_data_temp = input_data[0,i,0:500].reshape(1,1,500)
+	enc = enc_save.predict(input_data_temp, verbose=1)
+	print(enc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
