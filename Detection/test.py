32f6d05ff1d4b4bf7de8f92f1ff477caeec85e67
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2021-06-25 15:47:53 +0900

    Update test.py

--- Detection/test.py
+++ Detection/test.py
@@ -8,82 +8,127 @@ from keras.layers import Input
 from tensorflow import keras
 from pandas import read_csv
 
-### Parameter Variables ###
+
+###################### Parameter Variables ######################
+
 FFT_Hz = 500
-input_data_min = 4500
-input_data_max = 5500
-test_data_min = 7750
-test_data_max = 8000
+train_min = 4500
+train_max = 5500
+test_min = 7750
+test_max = 8000
 epoch = 1
 
-### Preparing Raw Data ###
+#################################################################
+
+
+################ Loading and Normalizing Raw Data ################
+
+#Loading Data
 data = read_csv("Data.csv", header=None)
-# Data normalization
-data_norm = np.array(data)
-data_norm = data_norm[:,0:FFT_Hz]
-data_max, data_min = data_norm.max(), data_norm.min()
-max_min_diff = data_max-data_min
-data_norm = (data_norm/max_min_diff)*100
-# Reshape data to fit into format
-timesteps, n_features = data_norm.shape
-data_norm = data_norm.reshape(timesteps, n_features, 1)
-input_samples = input_data_max - input_data_min
-test_samples = test_data_max - test_data_min
+data = np.array(data)
 
-### Preprocessing Data ###
-# Input data
-input_data = data_norm[input_data_min:input_data_max, :, 0]
-np.random.shuffle(input_data)
-input_data = input_data.reshape(input_samples, FFT_Hz, 1)
+#Stating necessary variables
+total_samples = data.shape[0]
+train_samples = train_max - train_min
+test_samples = test_max - test_min
 
-# Test data
-test_data = data_norm[test_data_min:test_data_max, :, 0]
-np.random.shuffle(test_data)
-test_data = test_data.reshape(test_samples, FFT_Hz, 1)
+#Preparation for FFT data normalization
+data_FFT = data[:, 0:FFT_Hz]
+data_FFT_max, data_FFT_min = data_FFT.max(), data_FFT.min()
+data_FFT_max_min_diff = data_FFT_max - data_FFT_min
+#Slip data into binary classes
+for i in range(total_samples):
+	if data[i,500] != 0.0:
+		data[i,500] = 1.0
 
-### Defining Model ###
-### Model is a LSTM combined with MLP for binary slip classification purposes ###
-# Defining input shape
-visible = Input(shape=(n_features, 1))
-# LSTM encoder of shapes 128 and 64
-encoder = LSTM(128, activation='relu', input_shape=(n_features, 1), return_sequences=True)(visible)
-encoder = LSTM(64, activation='relu', return_sequences=True)(encoder)
+##################################################################
 
-# Reconstruction decoder of shapes 64 and 128
-#decoder = RepeatVector(64)(encoder)
-decoder = LSTM(64, activation='relu', return_sequences=True)(encoder)
-decoder = LSTM(128, activation='relu', return_sequences=True)(decoder)
-# Organizing output back into the shape of input
-decoder = TimeDistributed(Dense(1))(decoder)
 
-# Tying encoder and decoder into one model
-model = keras.Model(inputs=visible, outputs=decoder)
+###################### Preprocessing Data ########################
+
+#Declaring training data
+training_data = data_FFT[train_min:train_max, :]
+#np.random.shuffle(training_data)
+
+#Declaring x_train and y_train data
+x_train_data, y_train_data = training_data[train_min:train_max, :], training_data[train_min:train_max, 500]
+
+#Data normalization for x_train data
+x_train_data = (x_train_data/data_FFT_max_min_diff)*100
+
+#Reshaping x_train and y_train data
+x_train_data = x_train_data.reshape(train_samples, FFT_Hz, 1)
+y_train_data = y_train_data.reshape(train_samples, 1, 1)
+
+#Declaring test data
+test_data = data_FFT[test_min:test_max, :]
+#np.random.shuffle(test_data)
+
+#Declaring x_test and y_test data
+x_test_data, y_test_data = test_data[test_min:test_max, :], test_data[test_min:test_max, 500]
+
+#Data normalization for x_test data
+x_test_data = (x_test_data/data_FFT_max_min_diff)*100
+
+#Reshaping x_test and x_test data
+x_test_data = x_test_data.reshape(test_samples, FFT_Hz, 1)
+y_test_data = y_test_data.reshape(test_samples, 1, 1)
+
+##################################################################
+
+
+######################## Defining Model ##########################
+
+#Model is a LSTM combined with MLP for binary slip classification purposes
+
+#Defining input shape
+visible = Input(shape=(FFT_Hz, 1))
+
+#LSTM model of shapes 256, 128 ,64, 1
+LSTM_model = LSTM(256, activation='relu', input_shape=(FFT_Hz, 1), return_sequences=True)(visible)
+LSTM_model = LSTM(128, activation='relu', return_sequences=True)(LSTM_model)
+LSTM_model = LSTM(64, activation='relu', return_sequences=True)(LSTM_model)
+LSTM_model = TimeDistributed(Dense(1))(LSTM_model)
+
+#MLP model of shapes 1024, 512, 256 and 1
+MLP_model = Dense(1024, activation='relu', input_shape(FFT_Hz, 1))(LSTM_model)
+MLP_model = Dropout(0.3)(MLP_model)
+MLP_model = Dense(256, activation='relu')(MLP_model)
+MLP_model = Dropout(0.3)(MLP_model)
+MLP_model = Dense(64, activation='relu')(MLP_model)
+MLP_model = Dropout(0.3)(MLP_model)
+MLP_model = Dense(1, activation='softmax')(MLP_model)
+
+#Compiling model
+model = keras.Model(inputs=visible, outputs=MLP_model)
 model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-# Printing a summary of the model
+
+#Printing a summary of the model
 model.summary()
 
-### Fitting model with the same input and output data ###
-history = model.fit(input_data, input_data, epochs = epoch, verbose=1)
+#################################################################
 
-### Plotting history ### 
-# Plotting loss
+
+################# Fitting and Evaluating Model ##################
+
+#Fitting model and saving into history
+history = model.fit(x_train_data, y_train_data, epochs = epoch, verbose=1)
+
+#Plotting loss
 plt.plot(history.history['loss'])
 plt.title('Model Loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.show()
 
-# Evaluating the results qualitatively
-for i in range(5):
-        test_data_temp = input_data[i+4500, 0:500]
-        test_data_temp = test_data_temp.reshape((1, 1, n_features))
-        yhat = model.predict(test_data_temp, verbose=1)
-        test_data_yhat = np.column_stack((input_data[0:500, i+4500], yhat[0,0:500]))
+#Evaluating model with test data
+evaluation = model.evaluate(x_test_data, y_test_data)
 
-# Saving the encoder
-enc_save = keras.Model(inputs = visible, outputs = encoder)
-enc_save.save('encoder.h5')
+##################################################################
 
-test = enc_save.predict(test_data_temp)
-print(np.shape(test))
-print(test)
+
+
+
+
+
+
